3e02f1776c31bec0bd6ea6c4a8673ac5e92762fc
Lya Roux <lya.roux@pm.me>
2023-10-30 10:21:57 +0100

    solved c01/ex01

---
+++ c00-c07/c01/ex01/README.md
@@ -0,0 +1,1 @@
+* this function creates a chain of * pointers pointing to 42.
---
+++ c00-c07/c01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,24 @@
+void ft_ultimate_ft(int******** number) {
+	********number = 42;
+}
+
+/*
+#include <stdio.h>
+int main(void) {
+
+	int nbr;
+	int *nbr1 = &nbr;
+	int **nbr2 = &nbr1;
+	int ***nbr3 = &nbr2;
+	int ****nbr4 = &nbr3;
+	int *****nbr5 = &nbr4;
+	int ******nbr6 = &nbr5;
+	int *******nbr7 = &nbr6;
+	int ********nbr8 = &nbr7;
+
+	ft_ultimate_ft(nbr8);
+	
+	printf("%d", nbr);
+
+}
+*/
